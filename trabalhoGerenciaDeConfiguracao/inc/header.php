ec1919f66a91a59a76715dd6cf90a486e19d57ba
Jéssica Bernardi Petersen <jessica041ster@gmail.com>
2020-08-27 15:12:46 -0300

    #1.0.0 - crud pedido
    Crud do pedido

---
+++ trabalhoGerenciaDeConfiguracao/inc/header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Gerencia de Configuração</title>
+
+        <script src="script.js" type="text/javascript"></script>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+
+        <link rel="icon" href="favicon.ico" type="image/favicon-icon" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/favicon-icon" />
+
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom fonts for this template -->
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="../js/script.js" type="text/javascript"></script>
+		
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+
+        <header>
+            <nav class="navbar navbar-expand-md navbar-light fixed-top navbar-dark bg-dark">
+                <div class="container">
+                    <a href="../index.php" class="navbar-brand">Konoha informática</a>
+                    <button class="navbar-toggler" data-target="#nav-principal" data-toggle="collapse">
+                        <i class="fas fa-bars text-white"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="nav-principal">
+                        <ul class="navbar-nav ml-auto">
+<!--                            <li class="nav-item">
+                                <a href="" class="nav-link">Pedidos</a>
+                            </li>
+                            <li class="nav-item divisor"></li>
+                            <li class="nav-item">-->
+                            <a href="../produto/produto.php" class="nav-link">Produto</a>
+                            </li>
+<!--                            <li class="nav-item divisor"></li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link">Clientes</a>
+                            </li>-->
+                        </ul>
+                    </div> <!-- fim navbar -->
+                </div><!-- Fim container -->
+            </nav>
+        </header>
+        <main class="container">
